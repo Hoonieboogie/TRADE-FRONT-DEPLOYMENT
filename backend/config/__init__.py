0c920afd9d6e320ad809506ff421c581758d2cb3
HoonHan <hanhoon08@gmail.com>
2025-12-11 11:37:20 +0900

    AWS 백엔드 배포 설정

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,6 +1,5 @@
 import pymysql
 
+# Django 5.x+ 버전 체크를 통과하기 위해 PyMySQL 버전을 mysqlclient 버전으로 위장
+pymysql.version_info = (2, 2, 7, "final", 0)
 pymysql.install_as_MySQLdb()
-
-# Django의 mysqlclient 버전 체크 우회
-pymysql.version_info = (2, 2, 1, "final", 0)
